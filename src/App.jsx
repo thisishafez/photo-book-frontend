4a89047657d2482a72aaafb498d64ad27274e9ee
Hafez <thisishafez@gmail.com>
2026-08-12 17:14:47 +0330

    Clean starter project - remove default Vite files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      fontFamily: 'Arial, sans-serif',
+      fontSize: '2rem'
+    }}>
+      <h1>Photo Book App</h1>
+    </div>
+  )
+}
+
+export default App
